c0d6cc377f87ade9f8e44b47333c0f52b2188b76
Bhagwaan <bhagban.housivity.com>
2026-09-01 15:26:25 +0530

    feat: add Mongoose dependency and initialize DBModule in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DomainModule } from './domains/domain.module';
 import { EnvConfig } from './configs/env.config';
+import { DBModule } from './db/db.config';
 
 @Module({
-  imports: [EnvConfig, DomainModule],
+  imports: [EnvConfig, DBModule, DomainModule],
   controllers: [],
   providers: [],
 })
