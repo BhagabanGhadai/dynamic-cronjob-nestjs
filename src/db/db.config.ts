602476a4a00f8df914e08f18f8919d38d9a624f1
Bhagwaan <bhagban.housivity.com>
2026-09-01 16:39:53 +0530

    feat: configure MongooseModule with dynamic MongoDB URI from ConfigService

--- src/db/db.config.ts
+++ src/db/db.config.ts
@@ -1,5 +1,18 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
-@Module({})
+@Module({
+    imports: [
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                uri: config.get<string>("MONGODB_URI")
+            })
+
+        })
+    ]
+})
 
 export class DBModule { }
