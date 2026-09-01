e4b500b4e1780c1c0e7085d91cd3a284f0e7e0da
Bhagwaan <bhagban.housivity.com>
2026-09-01 16:44:45 +0530

    feat: implement CronController and register it in CronModule

--- src/domains/crons/cron.module.ts
+++ src/domains/crons/cron.module.ts
@@ -1,7 +1,11 @@
 import { Module } from "@nestjs/common";
+import { CronController } from "./cron.controller";
 
 @Module({
-    imports: []
+    imports: [],
+    controllers: [CronController],
+    providers: [],
+    exports: []
 })
 
 export class CronModule { }
