36907fa2c376baffcb9a278e3d3b6e5407405783
Bhagwaan <bhagban.housivity.com>
2026-09-01 18:23:35 +0530

    feat: register Task entity, repository, and service in TaskModule

--- src/domains/tasks/task.module.ts
+++ src/domains/tasks/task.module.ts
@@ -1,12 +1,15 @@
 import { Module } from "@nestjs/common";
 import { TaskController } from "./task.controller";
 import { MongooseModule } from "@nestjs/mongoose";
+import { Task, TaskSchema } from "./task.entity";
+import { TaskRepository } from "./task.repository";
+import { TaskService } from "./task.service";
 
 @Module({
-    imports: [MongooseModule.forFeature([])],
+    imports: [MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }])],
     controllers: [TaskController],
-    providers: [],
-    exports: []
+    providers: [TaskService, TaskRepository],
+    exports: [TaskService, TaskRepository]
 })
 
 export class TaskModule { }
