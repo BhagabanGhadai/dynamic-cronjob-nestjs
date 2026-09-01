b3328b5d267fdf15935b6257f44bd7cd46b7e12f
Bhagwaan <bhagban.housivity.com>
2026-09-01 15:17:25 +0530

    chore: downgrade nestjs dependencies and disable application abort on error

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
+    abortOnError: false
+  });
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
