a4d931270c186067311baa9bd309585f5cef61cf
Bhagwaan <bhagban.housivity.com>
2026-09-02 10:01:58 +0530

    feat: initialize ScheduleModule and register CronModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DomainModule } from './domains/domain.module';
 import { EnvConfig } from './configs/env.config';
 import { DBModule } from './db/db.config';
+import { CronModule } from './crons';
 
 @Module({
-  imports: [EnvConfig, DBModule, DomainModule],
+  imports: [EnvConfig, DBModule, DomainModule, CronModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
